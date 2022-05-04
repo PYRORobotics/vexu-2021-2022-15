4f39287e451f91986f314a0f23ebfd0c657547e2
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2022-05-04 13:57:04 -0700

    changes for jaws and lift positions

--- src/pyrolib/jaws.cpp
+++ src/pyrolib/jaws.cpp
@@ -5,12 +5,14 @@
 #include "../../include/pyrolib/include/jaws.h"
 
 #include <utility>
+
 namespace pyro {
-    jaws::jaws(okapi::Motor jawsMotor, pros::ADIDigitalIn limit, pros::ADIDigitalIn jawsTrigger, double target_pos) : jawsMotor(std::move(jawsMotor)),
-                                                                                                  jawsTrigger(jawsTrigger),
-                                                                                                  limit(limit),
-                                                                                                  state(UNKNOWN),
-                                                                                                  triggered(false), open_pos(target_pos){
+    jaws::jaws(okapi::Motor jawsMotor, pros::ADIDigitalIn limit, pros::ADIDigitalIn jawsTrigger, double target_pos)
+            : jawsMotor(std::move(jawsMotor)),
+              jawsTrigger(jawsTrigger),
+              limit(limit),
+              state(UNKNOWN),
+              triggered(false), open_pos(target_pos) {
         jawsMotor.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
         jawsMotor.setGearing(okapi::AbstractMotor::gearset::red);
         jawsMotor.setEncoderUnits(okapi::AbstractMotor::encoderUnits::degrees);
@@ -20,12 +22,12 @@ namespace pyro {
     bool jaws::calibrate_task() {
         jawsMotor.moveVelocity(30);
         pros::delay(50);
-        while(jawsMotor.getCurrentDraw() > 100) {
+        while (jawsMotor.getCurrentDraw() > 100) {
             pros::delay(10);
         }
         jawsMotor.moveVoltage(-2000);
         pros::delay(150);
-        while(abs(jawsMotor.getActualVelocity()) > 1){
+        while (abs(jawsMotor.getActualVelocity()) > 1) {
             pros::delay(10);
         }
         pros::delay(500);
@@ -37,12 +39,12 @@ namespace pyro {
     }
 
     bool jaws::calibrate() {
-        pros::Task cal([this]{ this->calibrate_task(); });
+        pros::Task cal([this] { this->calibrate_task(); });
         return true;
     }
 
     bool jaws::open_task() {
-        if(state == CALIBRATED || state == CLOSED){
+        if (state == CALIBRATED || state == CLOSED) {
 //            int absolute_pos = jawsMotor.getPosition();
 //            int revs = (int) absolute_pos / 360;
 //            int relative_pos = (int) absolute_pos % 360;
@@ -59,22 +61,22 @@ namespace pyro {
 //            while(abs(jawsMotor.getActualVelocity()) > 0){
 //                pros::delay(10);
 //            }
-            while(!limit.get_new_press()) {
+            while (!limit.get_new_press()) {
                 jawsMotor.moveVelocity(90);
                 pros::delay(10);
             }
 
             jawsMotor.moveVelocity(0);
+            jawsMotor.moveRelative(20, 100);
             state = OPEN;
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
     bool jaws::open() {
-        pros::Task ope([this]{ this->open_task(); });
+        pros::Task ope([this] { this->open_task(); });
         return true;
     }
 
@@ -89,35 +91,35 @@ namespace pyro {
 //        else{
 //            return false;
 //        }
-        if(state == OPEN) {
-            long opened_draw = jawsMotor.getCurrentDraw();
-
-            while(opened_draw - jawsMotor.getCurrentDraw() < 200) {
-                pros::delay(200);
-                jawsMotor.moveVelocity(100);
-                printf("%ld\n", jawsMotor.getCurrentDraw());
-            }
-            jawsMotor.moveVelocity(0);
+        if (state == OPEN) {
+//            long opened_draw = jawsMotor.getCurrentDraw();
+//
+//            while(opened_draw - jawsMotor.getCurrentDraw() < 200) {
+//                pros::delay(10);
+//                jawsMotor.moveVelocity(100);
+//                printf("%ld\n", jawsMotor.getCurrentDraw());
+//            }
+//            jawsMotor.moveVelocity(0);
+            jawsMotor.moveRelative(100, 100);
             pros::delay(500);
-            jawsMotor.moveVoltage(-2000);
+            jawsMotor.moveVoltage(-2500);
             state = CLOSED;
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
     void jaws::toggle() {
-        if(state == OPEN) {
+        if (state == OPEN) {
             close();
-        } else if(state == CLOSED || state == CALIBRATED) {
+        } else if (state == CLOSED || state == CALIBRATED) {
             open();
         }
     }
 
     bool jaws::close() {
-        pros::Task clo([this]{ this->close_task(); });
+        pros::Task clo([this] { this->close_task(); });
         return true;
     }
 
@@ -129,10 +131,9 @@ namespace pyro {
     bool jaws::getNewTrigger() {
         //printf("button: %d, triggered: %d\n", jawsTrigger.get_value(), triggered);
         return jawsTrigger.get_new_press();
-        if(!triggered && isTriggered()){
+        if (!triggered && isTriggered()) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -141,7 +142,7 @@ namespace pyro {
         return jawsMotor.getTemperature();
     }
 
-    double jaws::getPosition(){
+    double jaws::getPosition() {
         return jawsMotor.getPosition();
     }
 
